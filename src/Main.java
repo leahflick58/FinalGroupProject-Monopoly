0f170a647ed3cda41aa83737a9107828141ec6b0
ANM4388 <mealyan20@gcc.edu>
2022-11-07 13:50:34 -0500

    added sout(propertyCards)

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         HashMap<String, Properties> propertyCards = new HashMap<>();
         propertyCards.put("Beans on Broad", new Properties(100, 500, 200));
+        System.out.println(propertyCards);
     }
 }
