261ae1c24604aad94dfc32c6b5e3e17e8ebfd53a
leahflick58 <flicklj19@gcc.edu>
2022-11-09 13:31:32 -0500

    Added ConsoleDriver class

---
+++ src/ConsoleDriver.java
@@ -0,0 +1,18 @@
+public class ConsoleDriver {
+    public static void main(String[] args) {
+        // game code should be modular
+        // bulk of logic elsewhere
+        // this should only have I/O logic
+        // meaning talk to user, get user input, show changes to user
+
+        // e.g.
+        // create an object of type Game
+        // Game g = new Game();
+        // maybe while(!g.hasWon())
+        // ask user for input
+        // call g.displayBoard()
+
+        // can add a gui with minimal if any changes to game logic
+
+    }
+}
