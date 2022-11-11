40a9dc14dd8aae6d4b8d47f9c42710ad925a116e
ANM4388 <mealyan20@gcc.edu>
2022-11-11 14:35:56 -0500

    Monopoly only has loop:GameLoop GameLoop class now has a board:Board so we can call Space action() in play method Added helper functions to GameLoop Changed collection types: Hashmap<Integer, Space> spaces; LL<Chance>; LL<Community> Added getSpaces() to Board class for use in play() in GameLoop

--- src/Monopoly.java
+++ src/Monopoly.java
@@ -2,12 +2,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Monopoly {
-    private Board board;
     private GameLoop loop;
 
     public Monopoly(ArrayList<String> players) {
-        board = new Board();        //ArrayLists <Spaces>,<Chance>, <CommunityChest>
-                                    //Board constructor should not take any args
         loop = new GameLoop(players);      //take player names
     }
 
