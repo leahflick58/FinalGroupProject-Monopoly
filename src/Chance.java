cc7cc5a68025830f27c582cd857cc44c5b7a232c
leahflick58 <flicklj19@gcc.edu>
2022-11-14 12:47:53 -0500

    Added types for Community and Chance

--- src/Chance.java
+++ src/Chance.java
@@ -1,7 +1,10 @@
 public class Chance extends Spaces{
     private String type;
-    public Chance(String type) {
+    // payBank, payPlayers, collectBank, collectPlayers, goToJail, getOutOfJail, homeImprovement
+    private int amount;
+    public Chance(String type, int amount) {
         this.type = type;
+        this.amount = amount;
     }
     @Override
     void action(Player p) {
--- src/Community.java
+++ src/Community.java
@@ -1,4 +1,12 @@
 public class Community extends Spaces {
+    String type;
+    // payBank, payPlayers, collectBank, collectPlayers, goToJail, getOutOfJail, repairs, advance
+    int amount;
+
+    public Community(String type, int amount) {
+        this.type = type;
+        this.amount = amount;
+    }
     @Override
     void action(Player p) {
 
