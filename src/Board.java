ebd95408c9760496f4f4f19aa27d2891c0fd7188
leahflick58 <flicklj19@gcc.edu>
2022-11-14 13:19:32 -0500

    Methods for Community and Chance for each card type. Only need to update whatever int Jail is

--- src/Board.java
+++ src/Board.java
@@ -3,9 +3,9 @@ import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Board {
-    private static HashMap<Integer, Spaces> spaces;
-    private LinkedList<Chance> chance;                   //Chance cards
-    private LinkedList<Community> communityChests;       //Community Chest cards
+    protected static HashMap<Integer, Spaces> spaces;
+    protected LinkedList<Chance> chance;                   //Chance cards
+    protected LinkedList<Community> communityChests;       //Community Chest cards
 
     public Board() {
         //TODO: complete constructor (+ Spaces, Chance, Community constructors)
--- src/Chance.java
+++ src/Chance.java
@@ -1,12 +1,12 @@
 public class Chance extends Spaces{
-    private String type;
+    private final String type;
     // payBank, payPlayers, collectBank, collectPlayers, goToJail, getOutOfJail, homeImprovement
-    private int amount;
+    private final int amount;
 
     /**
      *
      * @param type payBank, payPlayers, collectBank, collectPlayers, goToJail, getOutOfJail, homeImprovement
-     * @param amount
+     * @param amount dollars collected/paid, or in getOutOfJail adds one Get Out of Jail card to Player's inventory
      */
     public Chance(String type, int amount) {
         this.type = type;
@@ -14,6 +14,59 @@ public class Chance extends Spaces{
     }
     @Override
     void action(Player p) {
-
+        switch (this.type) {
+            case "payBank":
+                payBank(p, this.amount);
+            case "payPlayers":
+                payPlayers(p, this.amount);
+            case "collectBank":
+                collectBank(p, this.amount);
+            case "collectPlayers":
+                collectPlayers(p, this.amount);
+            case "goToJail":
+                goToJail(p);
+            case "getOutOfJail":
+                getOutOfJail(p);
+            case "homeImprovement":
+                homeImprovement(p);
+        }
     }
+
+    public void payBank(Player p, int amount) {
+        p.addOrSubBankBalance(-amount);
+    }
+
+    public void payPlayers(Player p, int amount) {
+        for (Player player : GameLoop.players) {
+            player.addOrSubBankBalance(amount);
+            p.addOrSubBankBalance(-amount);
+        }
+    }
+
+    public void collectBank(Player p, int amount) {
+        p.addOrSubBankBalance(amount);
+    }
+
+    public void collectPlayers(Player p, int amount) {
+        for (Player player : GameLoop.players) {
+            player.addOrSubBankBalance(-amount);
+            p.addOrSubBankBalance(amount);
+        }
+    }
+
+    public void goToJail(Player p) {
+        // TODO: Set this space to jail's actual int
+        p.setCurrentSpace(0);
+    }
+
+    public void getOutOfJail(Player p) {
+        p.setHasGetOutOfJail(p.hasGetOutOfJail + 1);
+    }
+
+    public void homeImprovement(Player p) {
+        p.addOrSubBankBalance(p.numberHotels() * 115);
+        p.addOrSubBankBalance(p.numberHouses() * 40);
+    }
+
+
 }
