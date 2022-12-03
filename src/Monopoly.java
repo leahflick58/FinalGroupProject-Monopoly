652e0245c4f3bb784c5d622c7b6f08aed4a47c23
ANM4388 <mealyan20@gcc.edu>
2022-12-03 12:13:39 -0500

    Changed original FinalGroupProject-Monopoly\Monopoly back to console version

--- src/Monopoly.java
+++ src/Monopoly.java
@@ -1,44 +1,16 @@
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class Monopoly extends JFrame {
+public class Monopoly {
     private GameLoop loop;
     private Player winner;
-    JPanel contentIncluder;
 
     /**
      * Creates a GameLoop for the Monopoly game and gets the winner
      * @param players as an ArrayList of String names
      */
     public Monopoly(ArrayList<String> players) {
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(100, 100, 450, 300);
-        setSize(1080,1000);
-        contentIncluder = new JPanel();
-        contentIncluder.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentIncluder);
-        contentIncluder.setLayout(null);
-
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setBorder(new LineBorder(new Color(0, 0,0)));
-        layeredPane.setBounds(6,6,632,630);
-        contentIncluder.add(layeredPane);
-
-        //board
-        //player1...player6
-
-        //Control Panel
-        JPanel rightPanel = new JPanel();
-        rightPanel.setBackground(Color.LIGHT_GRAY);
-        rightPanel.setBorder(new LineBorder(Color.BLACK));
-        rightPanel.setBounds(640,6,419,630);    //AM adjusted dimensions
-        contentIncluder.add(rightPanel);
-
         loop = new GameLoop(players);
         winner = loop.gameLoop();
     }
@@ -54,7 +26,6 @@ public class Monopoly extends JFrame {
     public static void main(String[] args) {
         Monopoly myGame = new Monopoly(new ArrayList<>(List.of("Alexis","David")));
         myGame.loop.gameLoop();
-        myGame.setVisible(true);
         System.out.println(myGame.winner.name + "won!");
     }
 
