8ead32c54cfbf84da0154ea22a0d6717531c1862
ANM4388 <mealyan20@gcc.edu>
2022-12-04 13:52:25 -0500

    Set up MonopolyGUI\GameLoop to display right console, added non-functional buttons and player assets card to visual

--- src/MonopolyGUI/GameLoop.java
+++ src/MonopolyGUI/GameLoop.java
@@ -1,11 +1,28 @@
 package MonopolyGUI;
 
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 
-public class GameLoop {
+public class GameLoop extends JFrame {
+    private GameLoop loop;
+    private Player winner;
+    private JPanel contentIncluder;
+    private JLayeredPane layeredPane;
+    private JPanel rightPanel;
+    private JButton btnBuy;
+    private JButton btnPayRent;
+    private JButton btnRollDice;
+    private JButton btnNextTurn;
+    private static JTextArea infoConsole;
+    private JPanel playerAssetsPanel;
+    private CardLayout c1 = new CardLayout();
+    private JTextArea panelPlayer1TextArea;
     public static ArrayList<Player> players;
     private Board board;
 
@@ -15,6 +32,72 @@ public class GameLoop {
      * @param names of the players in the game
      */
     public GameLoop(ArrayList<String> names) {
+        //@author
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 450, 300);
+        setSize(1080,700);
+        contentIncluder = new JPanel();
+        contentIncluder.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentIncluder);
+        contentIncluder.setLayout(null);
+
+        //@author
+        layeredPane = new JLayeredPane();
+        layeredPane.setBorder(new LineBorder(new Color(0, 0,0)));
+        layeredPane.setBounds(6,6,632,630);
+        contentIncluder.add(layeredPane);
+
+        //Control Panel
+        //@author
+        rightPanel = new JPanel();
+        rightPanel.setBackground(Color.LIGHT_GRAY);
+        rightPanel.setBorder(new LineBorder(new Color(0,0,0)));
+        rightPanel.setBounds(640,6,419,630);    //AM adjusted dimensions
+        contentIncluder.add(rightPanel);
+        rightPanel.setLayout(null);
+
+        btnBuy = new JButton("Buy");
+        btnBuy.setBounds(81,478,117,29);
+        rightPanel.add(btnBuy);
+        btnBuy.setEnabled(false);
+
+        btnPayRent = new JButton("Pay Rent");
+        btnPayRent.setBounds(210, 478, 117, 29);
+        rightPanel.add(btnPayRent);
+        btnPayRent.setEnabled(false);
+
+        btnRollDice = new JButton("Roll Dice");
+        btnRollDice.setBounds(81, 413, 246, 53);
+        rightPanel.add(btnRollDice);
+        btnRollDice.setEnabled(false);
+
+        btnNextTurn = new JButton("Next Turn");
+        btnNextTurn.setBounds(81, 519, 246, 53);
+        rightPanel.add(btnNextTurn);
+        btnNextTurn.setEnabled(false);
+
+        JPanel test = new JPanel();
+        test.setBounds(81, 312, 246, 68);
+        rightPanel.add(test);
+        test.setLayout(null);
+
+        infoConsole = new JTextArea();
+        infoConsole.setColumns(20);
+        infoConsole.setRows(5);
+        infoConsole.setBounds(6, 6, 234, 56);
+        test.add(infoConsole);
+        infoConsole.setLineWrap(true);
+        infoConsole.setText("PLayer 1 starts the game by clicking Roll Dice!");
+
+//        playerAssetsPanel = new JPanel();
+//        playerAssetsPanel.setBounds(81, 28, 246, 189);
+//        rightPanel.add(playerAssetsPanel);
+//        playerAssetsPanel.setLayout(c1);
+
+
+        setVisible(true);
+
+        ////////////////////////////
         board = new Board();        //ArrayLists <Spaces>,<Chance>, <CommunityChest>
         players = new ArrayList<>();
         for (String p : names) {
@@ -42,9 +125,34 @@ public class GameLoop {
      */
     public void play() {
         for(Player p : players) {
+            System.out.println("\n" + p.name + "'s Balance: $" + p.bankBalance);
+
+            //adapted from []
+            playerAssetsPanel = new JPanel();
+            playerAssetsPanel.setBounds(81, 28, 246, 189);
+            rightPanel.add(playerAssetsPanel);
+            playerAssetsPanel.setLayout(c1);
+
+            JPanel playerPanel = new JPanel();
+            playerPanel.setBackground(new Color(0,190,190));
+            playerAssetsPanel.add(playerPanel, "0");
+            playerPanel.setLayout(null);
+
+            JLabel playerTitle = new JLabel(p.name + "'s Assets");
+            playerTitle.setForeground(Color.WHITE);
+            playerTitle.setHorizontalAlignment(SwingConstants.CENTER);
+            playerTitle.setBounds(0, 6, 240, 16);
+            playerPanel.add(playerTitle);
+
+            panelPlayer1TextArea = new JTextArea();
+            panelPlayer1TextArea.setBounds(10, 34, 230, 149);
+            panelPlayer1TextArea.setText("Balance: $" + p.bankBalance);
+            playerPanel.add(panelPlayer1TextArea);
+
+            setVisible(true);
+
             if (!p.isBankrupt()) {
-                System.out.println("\n" + p.name + "'s Balance: $" + p.bankBalance);
-                                        //if p.isinJail && p.usesJailFreeCard {
+
                 if (!p.isInJail) {
                     int rolls = 0;
                     boolean doubles;
--- src/MonopolyGUI/Monopoly.java
+++ src/MonopolyGUI/Monopoly.java
@@ -10,7 +10,18 @@ import java.util.List;
 public class Monopoly extends JFrame {
     private GameLoop loop;
     private Player winner;
-    JPanel contentIncluder;
+    private JPanel contentIncluder;
+    private JLayeredPane layeredPane;
+    private JPanel rightPanel;
+    private JButton btnBuy;
+    private JButton btnPayRent;
+    private JButton btnRollDice;
+    private JButton btnNextTurn;
+    private static JTextArea infoConsole;
+    private JPanel playerAssetsPanel;
+    private CardLayout c1 = new CardLayout();
+    private JTextArea panelPlayer1TextArea;
+//    private JTextArea panelPlayer2TextArea;
 
     /**
      * Constructs the Monopoly GUI window, adapted from [PUT GITHUB REPOSITORY HERE]
@@ -18,38 +29,91 @@ public class Monopoly extends JFrame {
      * @param players as an ArrayList of String names
      */
     public Monopoly(ArrayList<String> players) {
-        /**
-         * @author
-         */
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(0, 0, 450, 300);
-        setSize(1080,700);
-        contentIncluder = new JPanel();
-        contentIncluder.setBorder(new EmptyBorder(5, 5, 5, 5));
-        setContentPane(contentIncluder);
-        contentIncluder.setLayout(null);
-
-        /**
-         * @author
-         */
-        JLayeredPane layeredPane = new JLayeredPane();
-        layeredPane.setBorder(new LineBorder(new Color(0, 0,0)));
-        layeredPane.setBounds(6,6,632,630);
-        contentIncluder.add(layeredPane);
-
-        //board
-        //player1...player6
-
-        //Control Panel
-        JPanel rightPanel = new JPanel();
-        rightPanel.setBackground(Color.LIGHT_GRAY);
-        rightPanel.setBorder(new LineBorder(Color.BLACK));
-        rightPanel.setBounds(640,6,419,630);    //AM adjusted dimensions
-        contentIncluder.add(rightPanel);
-
-        //
-
-        setVisible(true);
+//        //@author
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setBounds(100, 100, 450, 300);
+//        setSize(1080,700);
+//        contentIncluder = new JPanel();
+//        contentIncluder.setBorder(new EmptyBorder(5, 5, 5, 5));
+//        setContentPane(contentIncluder);
+//        contentIncluder.setLayout(null);
+//
+//        //@author
+//        layeredPane = new JLayeredPane();
+//        layeredPane.setBorder(new LineBorder(new Color(0, 0,0)));
+//        layeredPane.setBounds(6,6,632,630);
+//        contentIncluder.add(layeredPane);
+//
+//        //Control Panel
+//        //@author
+//        rightPanel = new JPanel();
+//        rightPanel.setBackground(Color.LIGHT_GRAY);
+//        rightPanel.setBorder(new LineBorder(new Color(0,0,0)));
+//        rightPanel.setBounds(640,6,419,630);    //AM adjusted dimensions
+//        contentIncluder.add(rightPanel);
+//        rightPanel.setLayout(null);
+//
+//        btnBuy = new JButton("Buy");
+//        btnBuy.setBounds(81,478,117,29);
+//        rightPanel.add(btnBuy);
+//        btnBuy.setEnabled(false);
+//
+//        btnPayRent = new JButton("Pay Rent");
+//        btnPayRent.setBounds(210, 478, 117, 29);
+//        rightPanel.add(btnPayRent);
+//        btnPayRent.setEnabled(false);
+//
+//        btnRollDice = new JButton("Roll Dice");
+//        btnRollDice.setBounds(81, 413, 246, 53);
+//        rightPanel.add(btnRollDice);
+//        btnRollDice.setEnabled(false);
+//
+//        btnNextTurn = new JButton("Next Turn");
+//        btnNextTurn.setBounds(81, 519, 246, 53);
+//        rightPanel.add(btnNextTurn);
+//        btnNextTurn.setEnabled(false);
+//
+//        JPanel test = new JPanel();
+//        test.setBounds(81, 312, 246, 68);
+//        rightPanel.add(test);
+//        test.setLayout(null);
+//
+//        infoConsole = new JTextArea();
+//        infoConsole.setColumns(20);
+//        infoConsole.setRows(5);
+//        infoConsole.setBounds(6, 6, 234, 56);
+//        test.add(infoConsole);
+//        infoConsole.setLineWrap(true);
+//        infoConsole.setText("PLayer 1 starts the game by clicking Roll Dice!");
+//
+//        playerAssetsPanel = new JPanel();
+//        playerAssetsPanel.setBounds(81, 28, 246, 189);
+//        rightPanel.add(playerAssetsPanel);
+//        playerAssetsPanel.setLayout(c1);
+//
+//        //Player 1
+//        //will change to for each player in loop:GameLoop
+//        JPanel panelPlayer1 = new JPanel();
+//        panelPlayer1.setBackground(Color.RED);
+//        playerAssetsPanel.add(panelPlayer1, "1");
+//        panelPlayer1.setLayout(null);
+//
+//        JLabel panelPlayer1Title = new JLabel("Player 1 Assets");
+//        panelPlayer1Title.setForeground(Color.WHITE);
+//        panelPlayer1Title.setHorizontalAlignment(SwingConstants.CENTER);
+//        panelPlayer1Title.setBounds(0, 6, 240, 16);
+//        panelPlayer1.add(panelPlayer1Title);
+//
+//        panelPlayer1TextArea = new JTextArea();
+//        panelPlayer1TextArea.setBounds(10, 34, 230, 149);
+//        panelPlayer1TextArea.setText("Put text here");
+//        panelPlayer1.add(panelPlayer1TextArea);
+//
+//
+//
+//
+//
+//        setVisible(true);
 
         loop = new GameLoop(players);
         winner = loop.gameLoop();
