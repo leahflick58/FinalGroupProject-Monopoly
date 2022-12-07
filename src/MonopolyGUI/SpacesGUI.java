5b8a2eed817cb2069cbe06eb2faf458c0baa6473
leahflick58 <flicklj19@gcc.edu>
2022-12-06 23:43:24 -0500

    fixed little labels on monopoly board

--- src/MonopolyGUI/SpacesGUI.java
+++ src/MonopolyGUI/SpacesGUI.java
@@ -15,52 +15,42 @@ public abstract class SpacesGUI extends JPanel{
         name = labelString;
         this.setLayout(null);
 
-        //if(rotationDegrees == 0) {
+        if (rotationDegrees == 135 || rotationDegrees == -135 || rotationDegrees == -45 || rotationDegrees == 45) {
+            // rotating a Jlabel: https://www.daniweb.com/programming/software-development/threads/390060/rotate-jlabel-or-image-in-label
+
+            nameLabel = new JLabel(name) {
+                protected void paintComponent(Graphics g) {
+                    Graphics2D g2 = (Graphics2D)g;
+                    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+                            RenderingHints.VALUE_ANTIALIAS_ON);
+                    AffineTransform aT = g2.getTransform();
+                    Shape oldshape = g2.getClip();
+                    double x = getWidth()/2.0;
+                    double y = getHeight()/2.0;
+                    aT.rotate(Math.toRadians(rotationDegrees), x, y);
+                    g2.setTransform(aT);
+                    g2.setClip(oldshape);
+                    super.paintComponent(g);
+                }
+            };
+                nameLabel.setBounds(0, 0, this.getWidth(), this.getHeight());
+
+
+            this.add(nameLabel);
+        } else {
             nameLabel = new JLabel(name);
             nameLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
             nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
             nameLabel.setBounds(0,10,this.getWidth(),10);
             this.add(nameLabel);
-//        } else {
-//            // rotating a Jlabel: https://www.daniweb.com/programming/software-development/threads/390060/rotate-jlabel-or-image-in-label
-//
-//            nameLabel = new JLabel(name) {
-//                protected void paintComponent(Graphics g) {
-//                    Graphics2D g2 = (Graphics2D)g;
-//                    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-//                            RenderingHints.VALUE_ANTIALIAS_ON);
-//                    AffineTransform aT = g2.getTransform();
-//                    Shape oldshape = g2.getClip();
-//                    double x = getWidth()/2.0;
-//                    double y = getHeight()/2.0;
-//                    aT.rotate(Math.toRadians(rotationDegrees), x, y);
-//                    g2.setTransform(aT);
-//                    g2.setClip(oldshape);
-//                    super.paintComponent(g);
-//                }
-//            };
-//            if(rotationDegrees == 90) {
-//                nameLabel.setBounds(5, 6, this.getWidth(), this.getHeight());
-//            }
-//            if(rotationDegrees == -90) {
-//                nameLabel.setBounds(-10, 0, this.getWidth(), this.getHeight());
-//            }
-//            if(rotationDegrees == 180) {
-//                nameLabel.setBounds(0, 0, this.getWidth(), this.getHeight());
-//            }
-//            if(rotationDegrees == 135 || rotationDegrees == -135 || rotationDegrees == -45 || rotationDegrees == 45) {
-//                nameLabel.setBounds(0, 0, this.getWidth(), this.getHeight());
-//            }
-            nameLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
-            nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
-
-            this.add(nameLabel);
-        //}
+        }
+        nameLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
+        nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
     }
 
     public void paintComponent (Graphics g) {
         super.paintComponent(g);
-        if(this.name.equals("Mediterranean Avenue") || this.name.equals("Baltic Avenenue")){
+        if(this.name.equals("Mediterranean Avenue") || this.name.equals("Baltic Avenue")){
             g.drawRect(0,this.getHeight()-10,this.getWidth(),10);
             g.setColor(new Color(61, 21, 21));
             g.fillRect(0,this.getHeight()-10,this.getWidth(),10);
